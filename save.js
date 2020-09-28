f2de36f5515f112e633fe60560eb4abc1865d46a
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-28 15:30:50 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,30 +1,11 @@
-function setup() { 
-  mycanvas = createCanvas(400, 400);
-} 
-
-iteration = 0;
-x = 0;
-v = 5;
-
-dt = 0.1;
-
-function draw() { 
-  background(255);
-  
-  x += v*dt;
-  
-  ellipse(x,0.5*height,50,50);
-  
-  formatted_number = FormatNumberLength(iteration-90,4);
-  saveCanvas(mycanvas,"screenshot"+formatted_number,"png");
-
-  iteration += 1;
+function setup() {
+  createCanvas(600,600);
+  background(50);
 }
 
-function FormatNumberLength(num, length) {
-    var r = "" + num;
-    while (r.length < length) {
-        r = "0" + r;
-    }
-    return r;
+function draw() {
+}
+
+function mouseClicked(){
+  save('myCanvas.png');
 }
