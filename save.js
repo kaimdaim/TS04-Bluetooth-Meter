21b1bf4c57eb9adbd2a4375aa0f7f8a0c4fecd2b
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-28 13:50:18 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,13 +1,13 @@
-<script>
-text("Okumayı kaydetmek için bas", 20, 20); 
 function setup() { 
 createCanvas(500, 300); 
-textSize(22); 
+textSize(11); 
 
-// Create a textarea for the input of reading
-inputArea = createElement("textarea"); 
+text("Click on the button below to save the written text", 20, 20); 
+
+// Create a textarea for the input of text 
+inputArea = createElement ("Reading: <output id='value'></label></output>"); 
 inputArea.position(30, 50); 
-inputArea.size(400, 120); 
+inputArea.size(400, 12); 
 
 // Create a button for saving text 
 saveBtn = createButton("Save text to file"); 
@@ -23,25 +23,3 @@ stringList = inputArea.value().split("\n");
 // Save the strings to file 
 save(stringList, "output_file.txt"); 
 } 
-
-  // handle incoming data:
-function handleData(event) {
-  // get the data buffer from the meter:
-  var buf = new Uint8Array(event.target.value.buffer);
-  // decode the results if this is the correct characteristic:
-  if (buf.length == 9) {
-    // decode the binary string:
-    decode(buf);
-    // from display.js, fill the HTML page:
-    fillDisplay(meter);
-
-  // Create a button for saving text 
-  saveBtn = createButton("Save text to file"); 
-  saveBtn.position(30, 200); 
-  saveBtn.mousePressed(saveFile); 
-} 
-
-  // Save the strings to file 
-  save(stringList, "output_file.txt"); 
-} 
-</script>
