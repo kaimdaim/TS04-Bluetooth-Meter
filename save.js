36956f3a594d4fa0a4c4d55ab6b8700fb458738b
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-28 15:27:31 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,15 +1,30 @@
-let photo;
+function setup() { 
+  mycanvas = createCanvas(400, 400);
+} 
 
-function preload() {
-  photo = loadImage('https://htmlpreview.github.io/?https://raw.githubusercontent.com/kaimdaim/TS04-Bluetooth-Meter/master/index.html ');
+iteration = 0;
+x = 0;
+v = 5;
+
+dt = 0.1;
+
+function draw() { 
+  background(255);
+  
+  x += v*dt;
+  
+  ellipse(x,0.5*height,50,50);
+  
+  formatted_number = FormatNumberLength(iteration-90,4);
+  saveCanvas(mycanvas,"screenshot"+formatted_number,"png");
+
+  iteration += 1;
 }
 
-function draw() {
-  image(photo, 0, 0);
-}
-
-function keyTyped() {
-  if (key === 's') {
-    photo.save('photo', 'jpg');
-  }
+function FormatNumberLength(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+        r = "0" + r;
+    }
+    return r;
 }
