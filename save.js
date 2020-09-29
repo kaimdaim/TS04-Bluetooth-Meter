0ef8fc5a92c49a278edbb00cac00a131b59da5c9
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-29 11:31:38 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,9 +1,11 @@
-// sets the value
-let inp;
-function setup() {
-  inp = createInput('Reading');
+let reading;
+function preload(){
+  reading = loadJSON('https://htmlpreview.github.io/?https://raw.githubusercontent.com/kaimdaim/TS04-Bluetooth-Meter/master/index.html');
 }
-
-function mousePressed() {
-  inp.value('Reading');
+function setup(){
+  createCanvas(200, 100);
+  let reading = reading.features[0].properties.mag;
+  let reading = reading.features[0].properties.place;
+  text(readingPlace, 0, height/2);
+  text(readingMag, 0, height-height/3);
 }
