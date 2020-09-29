1bf704c0f290286510882dbae19a581eeeb977a9
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-29 15:18:50 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,9 +1,46 @@
-function setup() {
-  createCanvas(400, 400);
-}
+switch (format) {
+        case space:
+        case csv:
 
-function draw() {
-  background(320);
-  console.log(reading);
- 
-}
+            if (timestamp) {
+                print_timestamp();
+                printf("%c", (format?',':' '));
+            }
+
+            print_measurement(measurement, decimal, scale);
+
+            if (show_units) {
+                printf("%c", (format?',':' '));
+                print_units(scale, function);
+                printf("%c", (format?',':' '));
+                print_type(reading[1]);
+            }
+            printf("\n");
+
+            break;
+
+        case json:
+
+            printf("{");
+
+            if (timestamp) {
+                printf("\"timestamp\":");
+                if (timestamp == date) printf("\"");
+                print_timestamp();
+                if (timestamp == date) printf("\"");
+                printf(", ");
+            }
+
+            printf("\"measurement\":");
+            print_measurement(measurement, decimal, scale);
+
+            if (show_units) {
+                printf(", \"units\":\"");
+                print_units(scale, function);
+                printf("\", \"type\":\"");
+                print_type(reading[1]);
+                printf("\"");
+            }
+            printf(" }\n");
+
+            break;
