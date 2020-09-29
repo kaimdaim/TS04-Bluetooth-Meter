85c8287b637dfab644e251643f1079324e28121d
kaimdaim <33600505+kaimdaim@users.noreply.github.com>
2020-09-29 11:34:10 +0300

    Update save.js

--- save.js
+++ save.js
@@ -8,4 +8,16 @@ function setup(){
   let reading = reading.features[0].properties.place;
   text(readingPlace, 0, height/2);
   text(readingMag, 0, height-height/3);
-}
+}// Create a button for saving text 
+  saveBtn = createButton("Save text to file"); 
+  saveBtn.position(30, 200); 
+  saveBtn.mousePressed(saveFile); 
+} 
+  
+function saveFile() { 
+  // Get the value of the textarea 
+  // Split according to nextline characters 
+  stringList = inputArea.value().split("\n"); 
+  
+  // Save the strings to file 
+  save(stringList, "output_file.txt"); 
